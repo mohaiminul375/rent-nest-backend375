1400b03f3b7ea673d5d9e3a4a88ed6b63b7979da
Mohaiminul375 <mohaiminul375@gmail.com>
2026-08-15 21:11:27 +0600

    user created successfully

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,6 +1,34 @@
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+import config from "../../config";
+
 const registerUserToDB = async (payload: any) => {
-    const { id, name, email, password, phone, role, status, address, createdAt, updateAt } = payload;
-    return payload;
+    const { name, email, password, phone, role, status, address } = payload;
+    const isExistedUser = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+    if (isExistedUser) {
+        throw new Error("User email already existed");
+    }
+    const hashedPassword = await bcrypt.hash(password, Number(config.bcrypt_salt_rounds))
+
+    const createUser = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+            phone,
+            role,
+            status,
+            address
+        },
+        omit: {
+            password: true
+        }
+    })
+    return createUser
 }
 
 
